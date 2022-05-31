682c3e73f71ad2c2637c562259f9de21974cbdb2
unnamedculture <106600929+unnamedculture@users.noreply.github.com>
2022-05-31 16:41:09 +0300

    Add files via upload

---
+++ main2.java
@@ -0,0 +1,23 @@
+package main_hilel;
+
+public class main2 {
+
+    public static void main(String[] args) {
+        byte a = 15;
+        byte b = 22;
+
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+
+        System.out.println("a= " + b);
+        System.out.println("b= " + a);
+
+        //50°26′01″ пн. ш. 30°19′42″ сх. д
+
+        float l = 50.2601F;
+        float w = 30.1942F;
+
+        System.out.println(l + "N");
+        System.out.println(w + "E");
+    }
+}
